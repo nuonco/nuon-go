55405820fcf1b80c4a80dd7b65fbd462486ccd5c
Jordan Acosta <jordanjacosta@gmail.com>
2023-09-25 16:37:39 -0700

    refactor: get api spec from API endpoint
    Now that the Open API spec is provided from the API itself, we can just
    read from there instead of generating locally and copy/pasting.

--- client.go
+++ client.go
@@ -14,7 +14,7 @@ import (
 
 //
 //go:generate -command swagger go run github.com/go-swagger/go-swagger/cmd/swagger
-//go:generate swagger generate client --skip-tag-packages -f ./swagger.json
+//go:generate swagger generate client --skip-tag-packages -f $NUON_API_URL/docs/doc.json
 //go:generate -command mockgen go run github.com/golang/mock/mockgen
 //go:generate mockgen -destination=mock.go -source=client.go -package=nuon
 type Client interface {
