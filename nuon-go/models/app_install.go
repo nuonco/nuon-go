4a526fa02a7314b68407f393dbeea4125058f08c
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-07 18:04:44 -0600

    chore: update to latest spec (#18)

--- nuon-go/models/app_install.go
+++ nuon-go/models/app_install.go
@@ -31,6 +31,9 @@ type AppInstall struct {
 	// created by id
 	CreatedByID string `json:"created_by_id,omitempty"`
 
+	// data plane ID
+	DataPlaneID string `json:"dataPlaneID,omitempty"`
+
 	// id
 	ID string `json:"id,omitempty"`
 
