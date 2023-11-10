36601e0f7ee6ba108734b1372555f02e3bc90878
Nat Hamilton <info@nathamilton.com>
2023-11-10 16:53:15 -0500

    chore: 0.8.4 release (#29)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- nuon-go/client/nuon_api_client.go
+++ nuon-go/client/nuon_api_client.go
@@ -18,14 +18,14 @@ var Default = NewHTTPClient(nil)
 const (
 	// DefaultHost is the default Host
 	// found in Meta (info) section of spec file
-	DefaultHost string = "localhost:8081"
+	DefaultHost string = "ctl.stage.nuon.co"
 	// DefaultBasePath is the default BasePath
 	// found in Meta (info) section of spec file
 	DefaultBasePath string = "/"
 )
 
 // DefaultSchemes are the default schemes found in Meta (info) section of spec file
-var DefaultSchemes = []string{"http"}
+var DefaultSchemes = []string{"https"}
 
 // NewHTTPClient creates a new nuon API HTTP client.
 func NewHTTPClient(formats strfmt.Registry) *NuonAPI {
