8f169b3cba809f95508f3bbf4662e2f408e6aee1
Jon Morehouse <morehousej09@gmail.com>
2024-04-20 18:47:15 -0600

    feat: introduce org invites

---
+++ org_users.go
@@ -0,0 +1,44 @@
+package nuon
+
+import (
+	"context"
+
+	"github.com/nuonco/nuon-go/client/operations"
+	"github.com/nuonco/nuon-go/models"
+)
+
+func (c *client) GetOrgInvites(ctx context.Context, limit *int64) ([]*models.AppOrgInvite, error) {
+	resp, err := c.genClient.Operations.GetOrgInvites(&operations.GetOrgInvitesParams{
+		Limit:   limit,
+		Context: ctx,
+	}, c.getOrgIDAuthInfo())
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Payload, nil
+}
+
+func (c *client) CreateOrgInvite(ctx context.Context, req *models.ServiceCreateOrgInviteRequest) (*models.AppOrgInvite, error) {
+	resp, err := c.genClient.Operations.CreateOrgInvite(&operations.CreateOrgInviteParams{
+		Req:     req,
+		Context: ctx,
+	}, c.getOrgIDAuthInfo())
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Payload, nil
+}
+
+func (c *client) CreateOrgUser(ctx context.Context, req *models.ServiceCreateOrgUserRequest) (*models.AppUserOrg, error) {
+	resp, err := c.genClient.Operations.AddUser(&operations.AddUserParams{
+		Req:     req,
+		Context: ctx,
+	}, c.getOrgIDAuthInfo())
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Payload, nil
+}
