e3352b877d7dd5a7fe5df1bf3744585ea692cae0
Jon Morehouse <MorehouseJ09@gmail.com>
2025-04-23 17:44:17 -0700

    chore: update sdk (#135)
    * feat: update sdk
    
    * chore: update

--- client/operations/operations_client.go
+++ client/operations/operations_client.go
@@ -110,6 +110,8 @@ type ClientService interface {
 
 	DeleteInstallComponents(params *DeleteInstallComponentsParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*DeleteInstallComponentsOK, error)
 
+	DeleteInstallSandbox(params *DeleteInstallSandboxParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*DeleteInstallSandboxOK, error)
+
 	DeleteInstaller(params *DeleteInstallerParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*DeleteInstallerOK, error)
 
 	DeleteOrg(params *DeleteOrgParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*DeleteOrgOK, error)
@@ -1937,6 +1939,45 @@ func (a *Client) DeleteInstallComponents(params *DeleteInstallComponentsParams, 
 	// unexpected success response
 	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
 	msg := fmt.Sprintf("unexpected success response for DeleteInstallComponents: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+/*
+DeleteInstallSandbox deletes an install component
+*/
+func (a *Client) DeleteInstallSandbox(params *DeleteInstallSandboxParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*DeleteInstallSandboxOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewDeleteInstallSandboxParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "DeleteInstallSandbox",
+		Method:             "DELETE",
+		PathPattern:        "/v1/installs/{install_id}/sandbox",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &DeleteInstallSandboxReader{formats: a.formats},
+		AuthInfo:           authInfo,
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*DeleteInstallSandboxOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for DeleteInstallSandbox: API contract not enforced by server. Client expected to get an error, but got: %T", result)
 	panic(msg)
 }
 
