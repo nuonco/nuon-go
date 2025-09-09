77552b142412d5cc60d9b821833ca90f764e05fd
fidiego <sayheytofred@gmail.com>
2025-09-09 15:10:40 -0500

    chore(generate): new endpoint for latest heart beat

--- client/operations/operations_client.go
+++ client/operations/operations_client.go
@@ -6178,6 +6178,45 @@ func (a *Client) GetLatestAppSecretsConfig(params *GetLatestAppSecretsConfigPara
 }
 
 /*
+GetLatestRunnerHeartBeat gets a runner
+*/
+func (a *Client) GetLatestRunnerHeartBeat(params *GetLatestRunnerHeartBeatParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*GetLatestRunnerHeartBeatOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewGetLatestRunnerHeartBeatParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "GetLatestRunnerHeartBeat",
+		Method:             "GET",
+		PathPattern:        "/v1/runners/{runner_id}/heart-beats/latest",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &GetLatestRunnerHeartBeatReader{formats: a.formats},
+		AuthInfo:           authInfo,
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*GetLatestRunnerHeartBeatOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for GetLatestRunnerHeartBeat: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+/*
 GetLogStream gets a log stream
 
 Return a log stream.
