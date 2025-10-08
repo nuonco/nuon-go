b475d31563e932a6430749f1aa3c8f414cde0890
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-10-07 22:18:16 -0500

    feat: new method: get workflow step approval contents (#217)
    * feat: new method: get workflow step approval contents
    
    * chore: decompress in app_transport and return interface{}

--- app_transport.go
+++ app_transport.go
@@ -1,6 +1,8 @@
 package nuon
 
 import (
+	"compress/gzip"
+	"io"
 	"net/http"
 )
 
@@ -18,7 +20,50 @@ func (t *appTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 		req.Header.Set("X-Nuon-Org-ID", t.orgID)
 	}
 
-	return t.transport.RoundTrip(req)
+	resp, err := t.transport.RoundTrip(req)
+	if err != nil {
+		return resp, err
+	}
+
+	// Handle gzip decompression if needed
+	if resp.Header.Get("Content-Encoding") == "gzip" {
+		gzipReader, err := gzip.NewReader(resp.Body)
+		if err != nil {
+			resp.Body.Close()
+			return nil, err
+		}
+
+		// Replace the response body with decompressed reader
+		resp.Body = &gzipReadCloser{
+			reader: gzipReader,
+			closer: resp.Body,
+		}
+
+		// Remove Content-Encoding header since we've decompressed
+		resp.Header.Del("Content-Encoding")
+		resp.Header.Del("Content-Length")
+		resp.ContentLength = -1
+		resp.Uncompressed = true
+	}
+
+	return resp, nil
+}
+
+// gzipReadCloser wraps a gzip reader and ensures both the gzip reader and original body are closed
+type gzipReadCloser struct {
+	reader io.Reader
+	closer io.Closer
+}
+
+func (g *gzipReadCloser) Read(p []byte) (n int, err error) {
+	return g.reader.Read(p)
+}
+
+func (g *gzipReadCloser) Close() error {
+	if rc, ok := g.reader.(io.Closer); ok {
+		rc.Close()
+	}
+	return g.closer.Close()
 }
 
 func (c *client) SetOrgID(orgID string) {
