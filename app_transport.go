19800f6d68c5d9b647b920f1fcade199cac35380
robisso <alphavibe@gmail.com>
2025-11-20 11:49:22 -0800

    feat: cli client version (#228)

--- app_transport.go
+++ app_transport.go
@@ -2,14 +2,16 @@ package nuon
 
 import (
 	"compress/gzip"
+	"fmt"
 	"io"
 	"net/http"
 )
 
 // appTransport is a transport that injects our authentication token and org id into the api request
 type appTransport struct {
-	authToken string
-	orgID     string
+	authToken     string
+	orgID         string
+	clientVersion string
 
 	transport http.RoundTripper
 }
@@ -18,6 +20,10 @@ func (t *appTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Set("Authorization", "Bearer "+t.authToken)
 	if t.orgID != "" {
 		req.Header.Set("X-Nuon-Org-ID", t.orgID)
+	}
+
+	if t.clientVersion != "" {
+		req.Header.Set("X-Nuon-Client-Version", t.clientVersion)
 	}
 
 	resp, err := t.transport.RoundTrip(req)
@@ -68,4 +74,8 @@ func (g *gzipReadCloser) Close() error {
 
 func (c *client) SetOrgID(orgID string) {
 	c.appTransport.orgID = orgID
+}
+
+func (c *client) SetClientVersion(version string) {
+	c.appTransport.clientVersion = fmt.Sprintf("nuoncli:%s", version)
 }
